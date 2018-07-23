ac78a107e38324eb9b1ea566907d89d73416559d
viet <viet.tranhoang@powergatesoftware.com>
2018-07-23 21:46:44 +0700

    add lib

--- review-company/src/app/app.module.ts
+++ review-company/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShareModule } from './share/share.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ShareModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ review-company/src/app/review/review.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReviewRoutingModule } from './review-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReviewRoutingModule
+  ],
+  declarations: []
+})
+export class ReviewModule { }
